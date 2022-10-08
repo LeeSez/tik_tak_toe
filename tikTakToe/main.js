a2e6860f9619fa458e36d559dbe71b6566499662
LeeSez <109472918+LeeSez@users.noreply.github.com>
2022-10-08 13:31:20 +0300

    The Full Game
    Added: The computer may now choose a color to go first, unlike the previous versions.
    Added: Clicking on a white square triggers a clicking sound, and the cursor changes to make it more clear for the user where they should click.
    Added: The "rain" of the winning animation now has two colors of "raindrops" according to the winner.
    Fixed: the entire row of raindrops would spawn in the middle of the screen first and only after few second relocated to a different location.
    Fixed: raindrops will no-longer appear on the edge of the screen.
    Upgraded: the start animation is now better as the compute takes few milliseconds more before marking the first to go player.

--- tikTakToe/main.js
+++ tikTakToe/main.js
@@ -1,18 +1,27 @@
 let clearRect,blueP, redP, mainContainer, gameContainer, divStartGame, rainContainer;
 let pStartGame, pReplayMessage;
-let turn = 0;
-let gameEnded = false;
-let squares = [];
-let rainArray = [];
-let shouldRain = false;
-let shouldBlink = false;
+
+let clickAudio;
+
+let turn = 0; //marks who's truen is it
+
+let squares = []; //keeps the reference to all white squares 
+let rainArray = []; //tracks the existing raindrops 
+
+let gameEnded = false; //stops futrthur palaying after the game was won
+let shouldRain = false; //stops the recalling of rain()
+let shouldBlink = false; //stops the recalling of blink()
+
 let red = [], blue = [];
 // turn = 0 - blue + x
 // turn = 1 - red + o
-let blueRGBA = [136,188,216,1], redRGBA = [255,101,66,1];
+
+let blueRGBA = [136,188,216,1], redRGBA = [255,101,66,1]; //keeping the colors values for use in blink()
 
 
-function init(){
+function init(){ 
+  //getting all neccessary references to initiate the game
+
   clearRect = document.getElementById("clearRect");
   mainContainer = document.getElementById("mainContainer");
   gameContainer = document.getElementById("gameContainer");
@@ -21,12 +30,15 @@ function init(){
   pStartGame = document.getElementById("pStartGame");
   divStartGame = document.getElementById("divStartGame");
   pReplayMessage = document.getElementById("pReplayMessage");
-  clearRect.style.visibility = "hidden";
 
+  clickAudio = document.createElement("audio");
+  clickAudio.src = "sound_effects/click.mp3";
 }
 
-function startAnimation(counter,width,height){
-  divStartGame.style.display = "none";
+function startAnimation(counter,width,height){ 
+  //the grow animation of gameContainer and and the creating of the white squares
+
+  divStartGame.style.display = "none"; //hides the start word
   
   gameContainer.style.width = width + "vw";
   gameContainer.style.height = height + "vh";
@@ -39,88 +51,91 @@ function startAnimation(counter,width,height){
       startAnimation(counter,width,height);
     },10);
   }
-  else{
+  else{ // initiates the game itself
     createSquares();
     setTimeout(()=>{
       clearRect.style.visibility = "visible";
       pReplayMessage.style.display = "block";
-      blueP.classList.add("blueHighlight");
     },500);
+    setTimeout(()=>{
+      chooseSide();
+    },1300);
   }
 }
 
-function createSquares(){
-  for(let i =0; i<9; i++){
-    let tempSquare = document.createElement("div");
-    tempSquare.className = "square";
-    tempSquare.i = i;
-    tempSquare.marked = "";
-    tempSquare.onclick = markSquare;
-    clearRect.appendChild(tempSquare);
-    squares.push(tempSquare);
+function chooseSide(){ 
+  //randomly selects a color to go first
+  if(Math.random()>0.49){ //blue 
+    blueP.classList.add("blueHighlight");
+    turn = 0;
+  }
+  else{ //red
+    redP.classList.add("redHighlight");
+    turn = 1;
   }
 }
 
-function deleteSquares(objectName){
-  if(clearRect.firstChild != null){
-    clearRect.removeChild(clearRect.firstChild);
-    return deleteSquares();
-  }
-}
+function markSquare(event){ 
+  //reacts when clikced on a white square, places the appropirate picture in the square and checks for wins of draw
 
-function createRainContainer(){
-  rainContainer = document.createElement("div");
-  rainContainer.id = "rainContainer";
-  document.body.appendChild(rainContainer);
-}
-
-function markSquare(event){
   if(event.target.marked == "" && !gameEnded){
-    if(turn == 0){
+    
+    if(turn == 0){ // x cases
+      clickAudio.pause();
+      clickAudio.play();
       createX(event.target);
       blue.push(event.target.i);
-      if(checkForWin(blue)){
+
+      if(checkForWin(blue)){ //win
         console.log("blue won");
         gameEnded = true;
         shouldRain = true;
         shouldBlink = true;
         createRainContainer();
-        rain(0,"images/grayX.png","images/grayX.png",10);
+        rain(0,"images/grayX.png","images/blueX.png",10);
         blueP.classList.remove("blueHighlight");
         blink(blueP,blueRGBA,false);
       }
-      else if(checkForDraw()){
+
+      else if(checkForDraw()){ //draw
         blueP.classList.remove("blueHighlight");
         shouldRain = true;
         createRainContainer();
         rain(0,"images/grayX.png","images/grayO.png",10);
       }
-      else{
+
+      else{ //regualr mark
       blueP.classList.remove("blueHighlight");
       redP.classList.add("redHighlight");
       turn = 1;
       }
     }
-    else{
+
+    else{ // o cases
+      clickAudio.pause();
+      clickAudio.play();
       createO(event.target);
       red.push(event.target.i);
-      if(checkForWin(red)){
+
+      if(checkForWin(red)){ //win
         console.log("red won");
         gameEnded = true;
         shouldRain = true;
         shouldBlink = true;
         createRainContainer();
-        rain(0,"images/grayO.png","images/grayO.png",10);
+        rain(0,"images/grayO.png","images/redO.png",10);
         redP.classList.remove("redHighlight");
         blink(redP,redRGBA,false);
       }
-      else if(checkForDraw()){
+
+      else if(checkForDraw()){ //draw
         redP.classList.remove("redHighlight");
         shouldRain = true;
         createRainContainer();
         rain(0,"images/grayX.png","images/grayO.png",10);
       }
-      else{
+
+      else{ // regular mark
       redP.classList.remove("redHighlight");
       blueP.classList.add("blueHighlight");
       turn = 0;
@@ -129,21 +144,9 @@ function markSquare(event){
   }
 }
 
-function createX(objectName){
-  let img = document.createElement("img");
-  img.src = "images/x.png";
-  objectName.appendChild(img);
-  objectName.marked = "x";
-}
+function checkForWin(arrayColor){ 
+  // lists all possible cases of a win, only used due to the little amount of cases and the persistante of them
 
-function createO(objectName) {
-  let img = document.createElement("img");
-  img.src = "images/o.png";
-  objectName.appendChild(img);
-  objectName.marked = "o";
-}
-
-function checkForWin(arrayColor){
   if(arrayColor.includes(0) && arrayColor.includes(4) && arrayColor.includes(8))
     return true;
   else if(arrayColor.includes(2) && arrayColor.includes(4) && arrayColor.includes(6))
@@ -164,7 +167,9 @@ function checkForWin(arrayColor){
   return false;
 }
 
-function checkForDraw(){
+function checkForDraw(){ 
+  //checkks if all the squares are marked to declair draw
+  
   for(let i = 0; i<squares.length; i++){
     if(!squares[i].marked)
       return false;
@@ -172,70 +177,9 @@ function checkForDraw(){
   return true;
 }
 
-function rain(counter,img1,img2, amountOfElements){
-  if(shouldRain){
-    for(let i = 0; i<rainArray.length; i++){
-      rainArray[i].reference.style.left = rainArray[i].x + "vw";
-      rainArray[i].reference.style.top = rainArray[i].y + "vh";
-      rainArray[i].y += 0.1;
-      rainArray[i].reference.style.opacity = 1-(rainArray[i].y*0.01)-0.1 +"";
-      if(rainArray[i].y > 90){
-        rainContainer.removeChild(rainArray[i].reference);
-        rainArray.splice(i,1);
-      }
-    }
-  
-    counter++;
-    if(counter == 100){
-      newRowOfRain(img1,img2,amountOfElements);
-      counter = 0;
-    }
-    setTimeout(()=>{rain(counter, img1,img2, amountOfElements);},10);
-  }
-}
+function restart(event){ 
+  //restarts all varibles and array of the model and reintitiates with new white squares 
 
-function newRowOfRain(img1,img2,number){
-  for(let i = 0; i<number; i++){
-    let tempImg = document.createElement("img");
-    tempImg.src = Math.random() > 0.49 ? img1 : img2;
-    tempImg.style.position = "absolute";
-    tempImg.style.top = -10+"px";
-    tempImg.style.zIndex = "3";
-    let tempObject = {
-      x:Math.floor((Math.random())*(100/number))+(100/number)*i,
-      y:-10,
-      reference: tempImg
-    };
-    if(tempObject.x>96)
-      tempObject.x = 96;
-    rainArray.push(tempObject);
-    rainContainer.appendChild(tempImg);
-  }
-}
-
-function blink(elementColor,arrayColor,goesUp){
-  if(shouldBlink){
-    let i = arrayColor[arrayColor.length-1];
-    if(goesUp){
-      i += 0.01;
-      arrayColor[arrayColor.length-1] = i;
-      if(i>1)
-        goesUp = false;
-    }
-    else{
-      i -= 0.01;
-      arrayColor[arrayColor.length-1] = i;
-      if(i<0.3)
-        goesUp = true;
-    }
-    elementColor.style.backgroundColor = "rgba("+arrayColor[0]+","+arrayColor[1]+","+arrayColor[2]+","+arrayColor[3]+")";
-    setTimeout(()=>{
-      blink(elementColor,arrayColor,goesUp);
-    },10);
-  }
-}
-
-function restart(event){
   if(event.keyCode == 32){
     rainArray = [];
     red = [];
@@ -246,15 +190,156 @@ function restart(event){
     shouldBlink = false;
     shouldRain = false;
 
-    turn = 0;
-
-    deleteSquares();
-    createSquares();
-    document.body.removeChild(rainContainer);
-
     redP.classList.remove("redHighlight");
     blueP.classList.remove("blueHighlight");
-    
-    blueP.classList.add("blueHighlight");    
+
+    setTimeout(()=>{
+      chooseSide();
+    },500);
+
+    deleteChildNodes(clearRect);
+    createSquares();
+    if(rainContainer != null) document.body.removeChild(rainContainer);    
+      
   }
+}
+
+
+//all element creating/deleting realated functions
+function createX(objectName){ 
+  //creates and element with the x image in the refered object 
+
+  let img = document.createElement("img");
+  img.src = "images/x.png";
+  objectName.appendChild(img);
+  objectName.marked = "x";
+}
+
+function createO(objectName){ 
+  //creates and element with the o image in the refered object 
+
+  let img = document.createElement("img");
+  img.src = "images/o.png";
+  objectName.appendChild(img);
+  objectName.marked = "o";
+}
+
+function createSquares(){ 
+  //creating and placing the squares in the clearRect
+
+  for(let i =0; i<9; i++){
+    let tempSquare = document.createElement("div");
+
+    tempSquare.className = "square";
+    tempSquare.i = i;
+    tempSquare.marked = "";
+    tempSquare.onclick = markSquare;
+
+    clearRect.appendChild(tempSquare);
+    squares.push(tempSquare);
+  }
+}
+
+function deleteChildNodes(object){ 
+  //deletes all child nodes of a given element
+
+  if(object.firstChild != null){
+    object.removeChild(clearRect.firstChild);
+    return deleteChildNodes(object);
+  }
+}
+
+
+//all animation related functions
+function blink(elementColor,arrayColor,goesUp){ 
+  //resposible for the winner blinking animation, changes the alpha var in the color and restores to the original when it should not run anymore
+
+  if(shouldBlink){
+    let alpha = arrayColor[arrayColor.length-1]; 
+
+    if(goesUp){
+      alpha += 0.01;
+      arrayColor[arrayColor.length-1] = alpha;
+      if(alpha>1)
+        goesUp = false;
+    }
+
+    else{
+      alpha -= 0.01;
+      arrayColor[arrayColor.length-1] = alpha;
+      if(alpha<0.3)
+        goesUp = true;
+    }
+
+    elementColor.style.backgroundColor = "rgba("+arrayColor[0]+","+arrayColor[1]+","+arrayColor[2]+","+arrayColor[3]+")";
+
+    setTimeout(()=>{
+      blink(elementColor,arrayColor,goesUp);
+    },10);
+  }
+
+  else{ //restores the original alpha val
+    blueP.style.backgroundColor = "rgba("+blueRGBA[0]+","+blueRGBA[1]+","+blueRGBA[2]+",0.3)";
+    redP.style.backgroundColor = "rgba("+redRGBA[0]+","+redRGBA[1]+","+redRGBA[2]+",0.3)";
+  }
+}
+
+function rain(counter,img1,img2, amountOfElements){ 
+  //maintainning the positions of all raindrops existing 
+
+  if(shouldRain){
+    for(let i = 0; i<rainArray.length; i++){
+      rainArray[i].reference.style.left = rainArray[i].x + "vw";
+      rainArray[i].reference.style.top = rainArray[i].y + "vh";
+      
+      rainArray[i].y += 0.1;
+      rainArray[i].reference.style.opacity = 1-(rainArray[i].y*0.01)-0.1 +"";
+      
+      if(rainArray[i].y > 90){ //if a raindrop hit the limit I specified (90vh) it would be removed 
+        rainContainer.removeChild(rainArray[i].reference);
+        rainArray.splice(i,1);
+      }
+    }
+  
+    counter++; //tracks one complete second when the page is running to avoid overcreations of raindrops
+    if(counter == 100){
+      newRowOfRain(img1,img2,amountOfElements);
+      counter = 0;
+    }
+
+    setTimeout(()=>{rain(counter, img1,img2, amountOfElements);},10);
+  }
+}
+
+function newRowOfRain(img1,img2,number){ 
+  //creates the raindrops elements in the rainContainer
+
+  for(let i = 0; i<number; i++){
+    let tempImg = document.createElement("img");
+    tempImg.src = Math.random() > 0.49 ? img1 : img2;
+
+    tempImg.style.position = "absolute";
+    tempImg.style.top = -10+"px";
+    tempImg.style.zIndex = "3";
+    
+    let tempObject = {
+      x:Math.floor((Math.random())*(100/number))+(100/number)*i,
+      y:-10,
+      reference: tempImg
+    };
+    
+    if(tempObject.x>96)
+      tempObject.x = 96;
+    
+    rainArray.push(tempObject);
+    rainContainer.appendChild(tempImg);
+  }
+}
+
+function createRainContainer(){ 
+  //create the container of the rain drops to easy delete when needed
+
+  rainContainer = document.createElement("div");
+  rainContainer.id = "rainContainer";
+  document.body.appendChild(rainContainer);
 }
